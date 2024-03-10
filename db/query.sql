fbd34c2614c9a8e208a9975d3633ba4571daa82d
Tamerbekir <astoryofame@gmail.com>
2024-03-10 12:25:39 -0400

    removed redundant SELECT syntax from query file and added SELECT file for roles where it joins the department roles id in the roles table so the department can get an id. Unsure if this coded is needed with other SELECT syntax in query.sql. May delete later. So far it is working but getting duplicate rows

--- db/query.sql
+++ db/query.sql
@@ -2,10 +2,9 @@
 SELECT employees.first_name, employees.last_name, roles.title, roles.salary -- TO SEE ALL DATA IN TABLE BY LINKING THE COLUMNS TOGETHER. First, selecting employee first/last name from employees table and role title/salary from roles table..
 FROM employees -- ..then, on the employee table..
 LEFT JOIN roles ON employees.role_id = roles.id -- ...the employees table will join (from left) the roles table with/ON the column role_id in the employees table which will then become the id column in the roles table....then..
-LEFT JOIN departments ON roles.department_id = departments.id -- ...the employees table will join (from left) the departments table with/ON the column role_id in the employees table which will then become the id column in the departments table..then..
+LEFT JOIN departments ON roles.department_id = departments.id -- ...the employees table will join (from left) the departments table with/ON the column role_id in the roles table which will then become the id column in the departments table..then..
 LEFT JOIN employees AS manager ON employees.manager_id = manager.id; -- the employees table will also be given the name manager with/ON the manager_id column in the employees table which will then become the id column for managers.
 
-SELECT roles.title, roles.salary, departments.name -- TO VIEW ROLE DATA. First, selecting title and salary in the roles table, then the name in the departments table...then..
-FROM roles -- ..from the roles table..
-JOIN departments ON roles.department_id = departments.id -- ..the roles table will join departments with/ON the departments_id in the roles table which will then be the id column in the departments table
-
+SELECT roles.id, roles.title, roles.salary, departments.name AS department  -- selecting id, salary and title from roles table, then 'name' column from departments table and calling it department
+FROM roles -- .. then from the roles table .. 
+LEFT JOIN departments ON roles.department_id = departments.id; -- ..the roles table will join the departments table with/ON the roles table with/ON the column department id which will become the department id on the departments table
